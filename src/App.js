e45e1f498a3d4e4b9bb1b4eca32383bb6633348b
IzzaSunii <akbarizza09@gmail.com>
2022-08-07 00:07:12 +0700

    cokcok

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App({}) {
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/Izza-ToDo">
+          <Route exact path="/Izza-Todo">
             <Layout>
               <MainPage />
             </Layout>
